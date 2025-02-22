ff359efa71552e227504b490903f4bdf310a47a2
Vedantghole06 <Vedantghole06@gmail.com>
2025-02-22 12:17:35 +0530

    Resolved merge conflict in App.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import ProjectDetails from './pages/ProjectDetails'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProjectDetails from "./pages/ProjectDetails";
+import LibraryTemplate from "./pages/LibraryTemplate";
+import CreateForm from "./pages/CreateForm";
+
+// import LoginForm from './pages/LoginForm'
+// import SignupForm from './pages/SignupForm'
 
 const App = () => {
   return (
     <>
-
       <Routes>
+        {/* <Route path="/" element={<LoginForm />} />
+        <Route path="/signup" element={<SignupForm />} /> */}
         <Route path="/" element={<Home />} />
+        <Route path="/create-form" element={<CreateForm />} />
+        <Route path="/library-template" element={<LibraryTemplate />} />
         <Route path="/project-details" element={<ProjectDetails />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
